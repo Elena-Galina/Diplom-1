e5e1c1b60b0ad6c3e706cfe725ece58fa8c7b11e
Elena-Galina <e.lena.galina@yandex.ru>
2023-04-30 14:05:44 +0300

    Changed files: pom.xml and BunTest.java

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,13 +1,34 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class BunTest {
 
-    String expectedName = DataGenerator.randomName();
-    float expectedPrice = DataGenerator.randomPrice();
+    private final String expectedName;
+
+    private final float expectedPrice;
+
+    public BunTest(String expectedName, float expectedPrice) {
+        this.expectedName = expectedName;
+        this.expectedPrice = expectedPrice;
+    }
+
+    @Parameterized.Parameters(name = "Тестовые данные: name=\"{0}\", price={1}")
+    public static Object[][] getBun() {
+        return new Object[][]{
+                {"", -100.50f},
+                {null, 0},
+                {"1234567890", 0.05f},
+                {"Яя", 100.00f},
+                {"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", Float.MAX_VALUE},
+                {"!@#$%^&*/:'=+-?<> _|();\"~`.,", 100},
+        };
+    }
 
     @Test
     public void testGetBunName() {
